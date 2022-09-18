715dc9613e6780bf0e3f666d5f6771e9f2dedbb8
Tobias Bechtold <tobias.bechtold@atruvia.de>
2022-09-18 02:40:05 +0200

    update canonical links

--- src/app/components/news-detail/news-detail.component.ts
+++ src/app/components/news-detail/news-detail.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {SeoService} from "../../services/seo.service";
 import {Article, ContentfulService} from 'src/app/services/contentful.service';
 import {Entry} from "contentful";
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-news-detail',
@@ -13,16 +14,22 @@ export class NewsDetailComponent implements OnInit {
 
   article: Entry<Article>;
 
-  constructor(private route: ActivatedRoute, private seoService: SeoService, private contentful: ContentfulService) {}
+  constructor(@Inject(DOCUMENT) private dom: any, private route: ActivatedRoute, private seoService: SeoService, private contentful: ContentfulService) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
+    var element: HTMLLinkElement= this.dom.querySelector(`link[rel='canonical']`)
 
     this.contentful.getArticle(id)
       .subscribe(article => {
         this.article = article.items.filter((item: any) => item.sys.id === id)[0];
         this.article.fields.image = article.includes.Asset.filter((item: any) => item.sys.id === this.article.fields.image.sys.id)[0];
         this.seoService.updateTitle(this.article.fields.title + ' - MT Performance eSport');
+        if (element) {
+          element.setAttribute('rel','canonical');
+          element.setAttribute('href', 'https://www.mt-performance-esport.de/news/' + this.article.sys.id);
+        }
+
         this.seoService.updateMetaTags([
           {property: 'og:title', content: this.article.fields.title},
           {
